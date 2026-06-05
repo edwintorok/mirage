98ba0b717657da312f2c1103386d34b4d74625a7
Edwin Török <edwin@tarides.com>
2026-06-05 17:16:13 +0100

    test/mirage/memory: test for Mirage_runtime.Memory.Private.try_alloc
    This is important, because an earlier version of this got
    completely away to `true` by GCC.
    
    Signed-off-by: Edwin Török <edwin@tarides.com>

--- test/mirage/memory/test_mem_limit.ml
+++ test/mirage/memory/test_mem_limit.ml
@@ -1,5 +1,12 @@
+open Mirage_runtime_memory
 open Test_common
 open Cmdliner
+
+let try_alloc_custom_bytes bytes =
+  try
+    alloc_custom_bytes (Bytes bytes) |> ignore_custom;
+    true
+  with Out_of_memory -> false
 
 let kib =
   let parser str =
@@ -27,11 +34,47 @@ let allocator_test amount_of_kib alloc_amount ignore_allocated =
         check_raises "OOM" Out_of_memory @@ alloc_kib limit_kib);
   ]
 
+let shifts = List.init 4 Fun.id
+
 let tests =
   [
     ( "alloc_custom_bytes",
       allocator_test bytes_of_kib alloc_custom_bytes ignore_custom );
     ("alloc_words", allocator_test words_of_kib alloc_words ignore_ocaml);
+    ( "try_alloc(small)",
+      [
+        gc_test_case "1" (fun _ ->
+            check' bool ~msg:"should succeed" ~expected:true
+              ~actual:(Private.try_alloc_bytes 1));
+      ] );
+    ( "try_alloc(large)",
+      (* this is important to test, some C compilers
+         might entirely optimize away the allocations from [try_alloc]
+       *)
+      [
+        gc_test_case "limit" (fun limit_kib ->
+            let (Bytes bytes) = limit_kib |> bytes_of_kib in
+            check' bool ~msg:"should fail" ~expected:false
+              ~actual:(Private.try_alloc_bytes bytes));
+      ] );
+    ( "try_alloc consistent",
+      shifts
+      |> List.map @@ fun shift ->
+         gc_test_case (string_of_int shift) @@ fun limit_kib ->
+         let (Bytes bytes) = limit_kib |> bytes_of_kib in
+         let bytes = bytes lsr shift in
+         let expected = Private.try_alloc_bytes bytes in
+         let actual = Private.try_alloc_bytes bytes in
+         check' bool ~msg:"consistent" ~expected ~actual );
+    ( "try_alloc matches try_alloc_custom",
+      shifts
+      |> List.map @@ fun shift ->
+         gc_test_case (string_of_int shift) @@ fun limit_kib ->
+         let (Bytes bytes) = limit_kib |> bytes_of_kib in
+         let bytes = bytes lsr shift in
+         let actual = Private.try_alloc_bytes bytes in
+         let expected = try_alloc_custom_bytes bytes in
+         check' bool ~msg:"consistent" ~expected ~actual );
   ]
 
 let () = run_with_args "test_mem_limit" ulimit_v_kib tests
