de078af6184e6b96397aecc64b0b3ca331f522ec
Edwin Török <edwin@tarides.com>
2026-06-10 13:30:33 +0100

    test/mirage/memory: ulimit -v test
    Set a limit on virtual memory size, and check that allocating
    beyond the limit fails, and allocating within the limit works.
    
    `ulimit -v` is OS specific, and know to fail on:
     OpenBSD (always EINVAL, not implemented)
     macOS (EINVAL for 10GiB, OK only for very large values like 100GiB)
    
    The test is only run on OSes where it is known to work (Linux and
    FreeBSD so far).
    
    Unikernels have a usually small amount of memory, and allocating
    beyond those limits would fail.
    To simulate that behaviour in a unit test environment `ulimit -v`
    can be used. Allocations beyond the limit make `malloc` and `mmap` fail,
    regardless of the kernel's overcommit setting, which can be used
    to study the GC's (and Mirage's) behaviour in a memory constrained
    environment.
    
    dune-project version had to be bumped for enabled-if+lib-available.
    
    Signed-off-by: Edwin Török <edwin@tarides.com>

---
+++ test/mirage/memory/test_common.ml
@@ -0,0 +1,64 @@
+type custom_heap =
+  (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+
+type ocaml_heap = int array
+type bytes = Bytes of int [@@unboxed]
+type words = Words of int [@@unboxed]
+type kib = KiB of int [@@unboxed]
+
+let word_size_in_bytes = Sys.word_size / 8
+let bytes_of_words (Words w) = Bytes (w * word_size_in_bytes)
+
+let words_of_bytes (Bytes b) =
+  Words ((b + word_size_in_bytes - 1) / word_size_in_bytes)
+
+let bytes_of_kib (KiB kib) = Bytes (kib lsl 10)
+let words_of_kib kib = kib |> bytes_of_kib |> words_of_bytes
+let pp_kib ppf (KiB kib) = Format.fprintf ppf "%d KiB" kib
+
+let alloc_custom_bytes (Bytes bytes) =
+  Bigarray.(Array1.create char c_layout bytes)
+
+let alloc_words (Words words) =
+  if words = 0 then [||]
+  else begin
+    (* [Array.make 0 0] would return the
+     empty array without allocating.
+     So we cannot allocate exactly 1 word.
+   *)
+    assert (words > 1);
+    (* the GC header uses 1 word *)
+    Array.make (words - 1) 0
+  end
+
+let ignore_custom (ba : _ Bigarray.Array1.t) =
+  ba |> Sys.opaque_identity |> ignore
+
+let ignore_ocaml (a : _ array) = a |> Sys.opaque_identity |> ignore
+
+let reset () =
+  (* [Gc.live_words] doc says that [Gc.full_major ()]
+     might have to be run twice, so always do that
+     at the beginning of a test
+   *)
+  Gc.full_major ();
+  Gc.compact ()
+
+open Alcotest.V1
+
+let gc_test_case name f =
+  test_case name `Quick @@ fun arg ->
+  reset ();
+  f arg
+
+let gc_heap_words () =
+  (* must run either a minor or major cycle,
+     otherwise the stats would be out-of-date (even [heap_words])
+   *)
+  Gc.minor ();
+  Words Gc.(quick_stat ()).heap_words
+
+let with_alive data f =
+  let r = f () |> Sys.opaque_identity in
+  let _keep = Sys.opaque_identity data in
+  r
---
+++ test/mirage/memory/test_common.mli
@@ -0,0 +1,66 @@
+type custom_heap
+(** shorter type for bigarrays (custom values) *)
+
+type ocaml_heap
+(** OCaml heap allocation *)
+
+(** {1 Units for memory allocations.}
+
+    To avoid accidentally mixing bytes, words and KiB (they're all integers)
+    introduce a separate type for each. Mark them unboxed to avoid runtime
+    overhead. *)
+
+type bytes = Bytes of int [@@unboxed]
+
+(** {!val:Sys.word_size} / 8 bytes *)
+type words = Words of int [@@unboxed]
+
+(** 1024 {!type:bytes} *)
+type kib = KiB of int [@@unboxed]
+
+val pp_kib : Format.formatter -> kib -> unit
+(** [pp_kib kib] prints [kib] in a human readable format. *)
+
+val bytes_of_words : words -> bytes
+(** [bytes_of_words w] is [w * Sys.word_size / 8]. *)
+
+val words_of_bytes : bytes -> words
+(** [words_of_bytes b] ensures that [w |> bytes_of_words |> words_of_bytes = w].
+*)
+
+val bytes_of_kib : kib -> bytes
+(** [bytes_of_kib kib] is [kib * 1024] *)
+
+val words_of_kib : kib -> words
+(** [words_of_kib kib] is [kib |> bytes_of_kib |> words_of_bytes] *)
+
+(** {1 Allocation helpers} *)
+
+val alloc_custom_bytes : bytes -> custom_heap
+(** [alloc_custom_bytes bytes] allocates [bytes] of {!type:custom} values. This
+    also allocates an OCaml value (the bigarray), so the total memory required
+    is larger than [bytes]. *)
+
+val alloc_words : words -> ocaml_heap
+(** [alloc_words words] allocates [words] on the OCaml heap. [words] cannot be
+    [1], and must be [>= 0]. *)
+
+val ignore_custom : custom_heap -> unit
+(** [ignore_custom custom] is a convenience function for ignoring a
+    {!type:custom_heap} value. Better than {!val:ignore}, doesn't ignore partial
+    applications. *)
+
+val ignore_ocaml : ocaml_heap -> unit
+(** [ignore_ocaml custom] is a convenience function for ignoring an
+    {!type:ocaml_heap} value. Better than {!val:ignore}, doesn't ignore partial
+    applications. *)
+
+val gc_test_case : string -> ('a -> unit) -> 'a Alcotest.V1.test_case
+(** [gc_test_case name f] is the testcase [f] with [name]. It runs the garbage
+    collector to compact the heap before each test. *)
+
+val gc_heap_words : unit -> words
+(** [gc_heap_words ()] is the size of the OCaml major heap in words *)
+
+val with_alive : 'a -> (unit -> 'b) -> 'b
+(** [with_alive data f] calls [f], keeping [data] alive across the call. *)
