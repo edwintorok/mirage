bc76655241d563e4f46e7a0866c703559adfbb13
Edwin Török <edwin@etorok.net>
2026-07-03 12:05:00 +0100

    wip

--- lib_runtime/mirage_runtime_memory.ml
+++ lib_runtime/mirage_runtime_memory.ml
@@ -4,10 +4,12 @@ module Private = struct
 
     external map : int -> t = "stub_reservation_map_noalloc" [@@noalloc]
 
-    external size_in_bytes : t -> int = "stub_reservation_size_in_bytes_noalloc" [@@noalloc]
+    external size_in_bytes : t -> int = "stub_reservation_size_in_bytes_noalloc"
+    [@@noalloc]
 
-    external unmap: t -> bool = "stub_reservation_unmap_noalloc" [@@noalloc]
+    external unmap : t -> bool = "stub_reservation_unmap_noalloc" [@@noalloc]
 
+    let empty = map 0
   end
 
   let try_alloc bytes =
@@ -15,4 +17,40 @@ module Private = struct
     let t = map bytes in
     let res = size_in_bytes t > 0 in
     res && unmap t
+
+  let reservation = Atomic.make Reservation.empty
+
+  let exchange_and_unmap_old t =
+    Reservation.unmap (Atomic.exchange reservation t)
+
+  let domain_count = 1 (* TODO *)
+  let ctrl = Atomic.make (Gc.get ())
+  let update_ctrl () = Atomic.set ctrl (Gc.get ())
+  let (_ : Gc.alarm) = Gc.create_alarm update_ctrl
+
+  let reserve_minor_heaps () =
+    (* finalizers need 3 words for [struct final] for every 2 words in the minor heap,
+       and there might also be some overhead when allocating small values
+       (~11% on OCaml 5.x (see sizeclasses.h), and ~18%(measured) on OCaml 4.x, so round up to 20%) 
+       1 + 3/2 + 1/5 = 27/10
+      *)
+    let bytes =
+      domain_count * (Atomic.get ctrl).minor_heap_size * 27 * Sys.word_size / 80
+    in
+    let t = Reservation.map bytes in
+    let ok = Reservation.size_in_bytes t > 0 in
+    (* only replace the reservation on a successful allocation *)
+    ok && exchange_and_unmap_old t
+
+  let safe_compact () =
+    (* Compaction has an implicit minor collection, which could crash
+       with [caml_fatal_error] if we are out of memory.
+       This can happen even if there'd be enough memory to free in the collection cycle.
+
+       Free the reservation first to ensure the minor collection doesn't crash.
+     *)
+    let (_ : bool) = exchange_and_unmap_old Reservation.empty in
+    Gc.compact ();
+    (* may fail *)
+    reserve_minor_heaps ()
 end
--- lib_runtime/mirage_runtime_memory.mli
+++ lib_runtime/mirage_runtime_memory.mli
@@ -1,33 +1,38 @@
 (**/**)
-module Private: sig
-    module Reservation: sig
-        type t
 
-        val size_in_bytes : t -> int
-        (** [size_in_bytes t] is the amount of bytes reserved.
+module Private : sig
+  module Reservation : sig
+    type t
 
-            @returns -1 if the reservation failed
-         *)
+    val size_in_bytes : t -> int
+    (** [size_in_bytes t] is the amount of bytes reserved.
 
-        val map : int -> t
-        (** [map bytes] reserves [bytes] using [mmap(2)].
-            Does not raise exceptions, and doesn't allocate OCaml values.
-            Use {!val:size_in_bytes} to check whether the reservation succeeded.
-        *)
+        @return -1 if the reservation failed *)
 
-        val unmap: t -> bool
-        (** [unmap reservation] frees memory associated with [reservation].
-            This is safe to call on failed reservations (it is a no-op and returns true).
-            Do not call this multiple times with the same [reservation] (double-free).
+    val map : int -> t
+    (** [map bytes] reserves [bytes] using [mmap(2)]. Does not raise exceptions,
+        and doesn't allocate OCaml values. Use {!val:size_in_bytes} to check
+        whether the reservation succeeded. *)
 
-            @returns [true] if unmapping succeeded, [false] if [munmap(2)] fails.
-        *)
-    end
+    val unmap : t -> bool
+    (** [unmap reservation] frees memory associated with [reservation]. This is
+        safe to call on failed reservations (it is a no-op and returns true). Do
+        not call this multiple times with the same [reservation] (double-free).
 
-    val try_alloc: int -> bool
-    (** [try_alloc bytes] attempts to allocate and immediately free [bytes].
-        Doesn't raise exceptions, and doesn't allocate OCaml values.
-    
-        @returns [true] if the allocation succeeded
-    *)
+        @return [true] if unmapping succeeded, [false] if [munmap(2)] fails. *)
+  end
+
+  val try_alloc : int -> bool
+  (** [try_alloc bytes] attempts to allocate and immediately free [bytes].
+      Doesn't raise exceptions, and doesn't allocate OCaml values.
+
+      @return [true] if the allocation succeeded *)
+
+  val reserve_minor_heaps : unit -> bool
+  (** [reserver_minor_heaps ()] reserves enough memory for a minor heap
+      collection. This reservation prevents bigarrays and other large OCaml
+      values from allocating and fragmenting this region. It'll be released when
+      we are low on memory
+
+      @return [true] if it succeeded. *)
 end
